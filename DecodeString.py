b87c9eafff5de512550d9ed4a239a8d36a1c0cac
Gautam-Balachandran <gautam.balachandran@gmail.com>
2024-06-27 10:41:46 -0400

    DFS - 2 Completed

---
+++ DecodeString.py
@@ -0,0 +1,43 @@
+﻿# Time Complexity : O(n)
+# Space Complexity : O(n)
+class Solution:
+    def decodeString(self, s: str) -> str:
+        count_stack = []
+        result_stack = []
+        current = []
+        k = 0
+
+        for ch in s:
+            if ch.isdigit():
+                k = k * 10 + int(ch)
+            elif ch == '[':
+                count_stack.append(k)
+                result_stack.append(current)
+                current = []
+                k = 0
+            elif ch == ']':
+                decoded_string = result_stack.pop()
+                repeat_times = count_stack.pop()
+                current = decoded_string + current * repeat_times
+            else:
+                current.append(ch)
+
+        return ''.join(current)
+
+# Example usage:
+sol = Solution()
+
+# Example 1
+s1 = "3[a]2[bc]"
+print("Input: {}".format(s1))
+print("Output: {}".format(sol.decodeString(s1)))
+
+# Example 2
+s2 = "3[a2[c]]"
+print("\nInput: {}".format(s2))
+print("Output: {}".format(sol.decodeString(s2)))
+
+# Example 3
+s3 = "2[abc]3[cd]ef"
+print("\nInput: {}".format(s3))
+print("Output: {}".format(sol.decodeString(s3)))
---
+++ NumberOfIslands.py
@@ -0,0 +1,57 @@
+﻿# Time Complexity : O(m*n)
+# Space Complexity : O(min(m,n))
+from collections import deque
+
+class Solution:
+    def numIslands(self, grid: list[list[str]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+
+        m, n = len(grid), len(grid[0])
+        count = 0
+        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # U, D, L, R
+        q = deque()
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    grid[i][j] = '0'
+                    count += 1
+                    q.append((i, j))
+                    while q:
+                        cur = q.popleft()
+                        for dir in dirs:
+                            nr, nc = cur[0] + dir[0], cur[1] + dir[1]
+                            if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == '1':
+                                q.append((nr, nc))
+                                grid[nr][nc] = '0'
+        return count
+
+# Example usage:
+sol = Solution()
+
+# Example 1
+grid1 = [
+    ["1","1","1","1","0"],
+    ["1","1","0","1","0"],
+    ["1","1","0","0","0"],
+    ["0","0","0","0","0"]
+]
+print(sol.numIslands(grid1)) # 1
+
+# Example 2
+grid2 = [
+    ["1","1","0","0","0"],
+    ["1","1","0","0","0"],
+    ["0","0","1","0","0"],
+    ["0","0","0","1","1"]
+]
+print(sol.numIslands(grid2)) # 3
+
+# Example 3
+grid3 = [
+    ["1","0","1","0","1"],
+    ["0","1","0","1","0"],
+    ["1","0","1","0","1"]
+]
+print(sol.numIslands(grid3)) # 8
